931897ae6c9fe56fbddfd96e526f08d7f6cac5dc
Your NameRyan <Rymaster95@gmail.com>
2018-03-29 19:36:13 -0400

    added more classes

--- ACM.BL/Order.cs
+++ ACM.BL/Order.cs
@@ -21,6 +21,37 @@ namespace ACM.BL
         public DateTimeOffset? OrderDate { get; set; }
         public int OrderId { get; private set; }
 
+        ///summary
+        ///retrieve one order.
+        ///summary
+        
+        public Order Retrieve(int orderId)
+        {
+            //Code that retrieves the defined order
+            return new Order();
+        }
 
+        /// summary
+        /// saves the current order.
+        /// summart
+        /// returns
+        
+        public bool Save()
+        {
+            //code that saves the defined order
+            return true;
+        }
+
+        ///summary 
+        ///Validates the order data
+        ///summary
+        ///returns
+
+        public bool Validate()
+        {
+            var isValid = true;
+            if (OrderDate == null) isValid = false;
+            return isValid;
+        }
     }
 }
